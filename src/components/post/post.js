0112fee3f151d98ad583a45038344a9771b1d0c4
Abdulsamet Kaya <aasametkaya@gmail.com>
2022-10-09 21:53:15 +0300

    post

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,9 +1,86 @@
 import React from "react";
+import {
+  Button,
+  InputAdornment,
+  OutlinedInput,
+  Snackbar,
+  CardHeader,
+  Typography,
+  CardContent,
+  Card,
+  Avatar,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import MuiAlert from "@material-ui/lab/Alert";
 
-
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div>
+      <Snackbar open={isSend} autoHideDuration={1200} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Your post is send!
+        </Alert>
+      </Snackbar>
+
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Link
+              to={{ pathname: "/users/" + userId }}
+              className={classes.link}
+            >
+              <Avatar aria-label="recipe" className={classes.avatar}>
+                {userName.charAt(0).toUpperCase()}
+              </Avatar>
+            </Link>
+          }
+          title={
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              multiline
+              placeholder="Title"
+              inputProps={{ maxLength: 25 }}
+              fullWidth
+              value={title}
+              onChange={(i) => handleTitle(i.target.value)}
+            ></OutlinedInput>
+          }
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              multiline
+              placeholder="Text"
+              inputProps={{ maxLength: 250 }}
+              fullWidth
+              value={text}
+              onChange={(i) => handleText(i.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <Button
+                    variant="contained"
+                    style={{
+                      background:
+                        "linear-gradient(45deg,#2196F3 30%, #21CBF3 90%)",
+                      color: "white",
+                    }}
+                    onClick={handleSubmit}
+                  >
+                    Post
+                  </Button>
+                </InputAdornment>
+              }
+            ></OutlinedInput>
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Post;
